65109cc4098ec3852cfdfec622107df455286ed2
Filippo Ranza <filipporanza@gmail.com>
2020-05-28 15:04:34 +0200

    imporve contribute  list UI

--- resources/views/private/contribute_list.blade.php
+++ resources/views/private/contribute_list.blade.php
@@ -5,28 +5,57 @@
 
 @section('body')
     <h2>My Active Contribute List</h2>
-    <ul>
+    <ul class="list-group">
         @foreach ($active_list as $list)
-            <li><a href="{{ route('list:manage', ['id' => $list->id]) }}">
-                {{ $list->name }}</a> - {{ $list->owner }} 
-                <form method="POST" action="{{ route('list:unsubscribe') }}" >
-                    @csrf
-                    <input type="hidden" value="{{ $list->id }}" name="list"> 
-                    @if ($list->poll)
-                        <input type="submit" value="Delete" disabled>
-                    @else
-                        <input type="submit" value="Delete">    
-                    @endif
-                </form>
+            <li class="list-group-item">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-6">
+                            <h4>
+                                <a href="{{ route('list:manage', ['id' => $list->id]) }}">
+                                    {{ $list->name }}
+                                </a>
+                            </h4>
+                            <ul>
+                                <li>Owner: {{ $list->owner }} </li>
+                            </ul>
+                        </div>
+                        <div class="col-3">
+                            <form method="POST" action="{{ route('list:unsubscribe') }}" >
+                                @csrf
+                                <input type="hidden" value="{{ $list->id }}" name="list"> 
+                                @if ($list->poll)
+                                    <input type="submit" class="btn btn-warning form-control" value="Unsubscribe" disabled>
+                                @else
+                                    <input type="submit" class="btn btn-warning form-control" value="Unsubscribe">    
+                                @endif
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </li>
         @endforeach
     </ul>
 
     <h2>My Archived Contribute List</h2>
-    <ul>
+    <ul class="list-group">
         @foreach ($archived_list as $list)
-            <li><a href="{{ route('list:old', ['id' => $list->id]) }}">
-                {{ $list->name }}</a> - {{ $list->owner }} 
+            <li class="list-group-item">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-6">
+                            <h4>
+                                <a href="{{ route('list:old', ['id' => $list->id]) }}">
+                                    {{ $list->name }}
+                                </a>
+                            </h4>
+                            <ul>
+                                <li>Owner: {{ $list->owner }} </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            
             </li>
         @endforeach
     </ul>
