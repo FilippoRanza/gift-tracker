2b6fb4ee3d2566299a6ffb063f7db67ed11a71d6
Filippo Ranza <filipporanza@gmail.com>
2020-05-28 16:30:09 +0200

    imporve debt managment UI

--- resources/views/private/debt_home.blade.php
+++ resources/views/private/debt_home.blade.php
@@ -5,30 +5,56 @@
 <hr>
 
 @if ($notify)
-    <div>
-        <a href="{{ route('debt:notification') }}">{{ $notify }} notifications</a>
+    <div class="alert alert-dark alert-dismissible fade show" role="alert"> 
+        <a href="{{ route('debt:notification') }}" type="button" class="btn btn-success">
+            @if ($notify == 1)
+                Notification
+            @else
+                Notifications
+            @endif
+            <span class="badge badge-light">{{ $notify }}</span>
+        </a>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
 
 
-<ul>
+<ul class="list-group">
     @foreach ($debts as $debt)
-        <li>
-            {{ $debt->name }} - € {{ $debt->debt->amount  / 100 }} 
-            <form method="POST" action="{{ route('debt:settle') }}">
-                @csrf
-                <input type="hidden" value="{{ $debt->debt->id }}" name="debt">
-                @if ($debt->debt->amount > 0)
-                    @if ($debt->marked)
-                        <input type="submit" value="Waiting for Confirm" disabled>
-                    @else
-                        <input type="submit" value="Mask as Settle">    
-                    @endif
-                    
-                @else
-                    <input type="submit" value="Settle">
-                @endif
-            </form>
+        <li class="list-group-item">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <h4>{{ $debt->name }}</h4>
+                        @if ($debt->debt->amount > 0)
+                            Debt: <span style="color:red">€ {{-$debt->debt->amount  / 100 }} </span>
+                        @else
+                            Credit: <span style="color:green">€ {{-$debt->debt->amount  / 100 }} </span>    
+                        @endif
+                        
+                    </div>
+                    <div class="col-3">
+                        <form method="POST" action="{{ route('debt:settle') }}">
+                            @csrf
+                            <input type="hidden" value="{{ $debt->debt->id }}" name="debt">
+                            @if ($debt->debt->amount > 0)
+                                @if ($debt->marked)
+                                    <input type="submit" class="btn btn-info form-control" value="Waiting for Confirm" disabled>
+                                @else
+                                    <input type="submit" class="btn btn-info form-control" value="Mask as Settle">    
+                                @endif
+                                
+                            @else
+                                <input type="submit" class="btn btn-primary form-control" value="Settle">
+                            @endif
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+           
         </li>
     @endforeach
 </ul>
--- resources/views/private/debt_notification.blade.php
+++ resources/views/private/debt_notification.blade.php
@@ -4,27 +4,40 @@
 
     @if (count($confirm))
         <h2>Confirm Settle</h2>        
-        <ul>
+        <ul class="list-group">
             @foreach ($confirm as $debt)
-                <li>
-                    {{ $debt->name }} - € {{ $debt->debt->amount / 100 }}
-                    <form method="POST" action="{{ route('debt:settle') }}">
-                        @csrf
-                        <input type="hidden" value="{{ $debt->debt->id }}" name="debt">
-                        <input type="submit" value="Accept Settle">
-                    </form>
-                    <form method="POST" action="{{ route('debt:refuse') }}">
-                        @csrf
-                        <input type="hidden" value="{{ $debt->debt->id }}" name="debt">
-                        <input type="submit" value="Refuse Settle">
-                    </form>
-                    <form method="POST" action="{{ route('debt:mark') }}">
-                        @csrf
-                        <input type="hidden" value="{{ $debt->debt->id }}" name="debt">
-                        <input type="submit" value="Mark as Seen">
-                    </form>
-                    
-                    
+                <li class="list-group-item">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-3">
+                                <h4>{{ $debt->name }}</h4>
+                                <span style="color: green">
+                                    € {{ -$debt->debt->amount / 100 }}
+                                </span>
+                            </div>
+                            <div class="col-3">
+                                <form method="POST" class="form-group" action="{{ route('debt:settle') }}">
+                                    @csrf
+                                    <input type="hidden" value="{{ $debt->debt->id }}" name="debt">
+                                    <input type="submit" class="btn btn-success form-control" value="Accept Settle">
+                                </form>
+                            </div>
+                            <div class="col-3">
+                                <form method="POST" class="form-group" action="{{ route('debt:refuse') }}">
+                                    @csrf
+                                    <input type="hidden" value="{{ $debt->debt->id }}" name="debt">
+                                    <input type="submit" class="btn btn-danger form-control" value="Refuse Settle">
+                                </form>
+                            </div>
+                            <div class="col-3">
+                                <form method="POST" class="form-group" action="{{ route('debt:mark') }}">
+                                    @csrf
+                                    <input type="hidden" value="{{ $debt->debt->id }}" name="debt">
+                                    <input type="submit" class="btn btn-secondary form-control" value="Mark as Seen">
+                                </form>
+                            </div>
+                        </div>
+                    </div>   
                 </li>
             @endforeach
         </ul>
@@ -32,17 +45,30 @@
 
     @if (count($refused))
         <h2>Refused Settle</h2>
+        <ul class="list-group">
            @foreach ($refused as $debt)
-            <li>
-                {{ $debt->name }} - € {{ $debt->debt->amount / 100 }}
-                <form method="POST" action="{{ route('debt:mark') }}">
-                    @csrf
-                    <input type="hidden" value="{{ $debt->debt->id }}" name="debt">
-                    <input type="submit" value="Mark as Seen">
-                </form>
-                
+            <li class="list-group-item">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3">
+                            <h4>{{ $debt->name }}</h4>
+                            <span style="color: red">
+                                € {{ $debt->debt->amount / 100 }}
+                            </span>
+                        </div>
+                        <div class="col-3">
+                            <form method="POST" class="form-group" action="{{ route('debt:mark') }}">
+                                @csrf
+                                <input type="hidden" value="{{ $debt->debt->id }}" name="debt">
+                                <input type="submit" class="btn btn-secondary form-control" value="Mark as Seen">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
             </li>
             @endforeach
+        </ul>
     @endif
 
 
