9181c3d71797a3a4eeb952c69b15042c20a9fe74
Filippo Ranza <filipporanza@gmail.com>
2020-05-28 14:50:03 +0200

    improve purchase list UI

--- resources/views/private/purchase_list.blade.php
+++ resources/views/private/purchase_list.blade.php
@@ -5,25 +5,43 @@
     <h1>{{ $user->name }}'s purchase list</h1>
     <hr>
 
-    <ul>
+    <ul class="list-group">
         @foreach ($purchases as $purchase)
-            <li>
-                <a href="{{ route('purchase:info', ['id' => $purchase->id]) }}">
-                    {{ $purchase->item }} from {{ $purchase->list }} to {{ $purchase->recipient }} - € {{ $purchase->price / 100 }}
-                </a>
-                <form action="{{ route('purchase:delete') }}" method="POST">
-                    @csrf
-                    <input type="hidden" value="{{ $purchase->id }}" name="purchase">
-                    <input type="submit" value="Delete">
-                </form>
-                <form action="{{ route('purchase:void') }}" method="POST">
-                    @csrf
-                    <input type="hidden" value="{{ $purchase->id }}" name="purchase">
-                    <input type="submit" value="Void">
-                </form>
+            <li class="list-group-item">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-8">
+                            <h4>{{ $purchase->item }}</h4>
+                            <ul>
+                                <li>For: {{ $purchase->recipient }}</li>
+                                <li>Price: € {{ $purchase->price / 100 }}</li>
+                            </ul>
+                            
+                            
+                        </div>
+                        <div class="col-4">
+                            <form class="form-group" action="{{ route('purchase:delete') }}" method="POST">
+                                @csrf
+                                <input type="hidden" value="{{ $purchase->id }}" name="purchase">
+                                <input type="submit" class="btn btn-danger form-control" value="Delete">
+                            </form>                             
+                            <form class="form-group" action="{{ route('purchase:void') }}" method="POST">
+                                @csrf
+                                <input type="hidden" value="{{ $purchase->id }}" name="purchase">
+                                <input type="submit" class="btn btn-warning form-control" value="Void">
+                            </form>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <a type="button" class="btn btn-primary form-control" href="{{ route('purchase:info', ['id' => $purchase->id ]) }}">Info</a>
+                        </div>
+                    </div>
+                </div>                          
             </li>
         @endforeach
     </ul>
 
 
 @endsection
+            
