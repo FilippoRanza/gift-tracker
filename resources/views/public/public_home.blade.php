0f925fd64f8d2a53754adaef02baa4760c7655e8
Filippo Ranza <filipporanza@gmail.com>
2021-02-16 15:11:25 +0100

    add some images

--- resources/views/public/public_home.blade.php
+++ resources/views/public/public_home.blade.php
@@ -8,7 +8,11 @@
   @include('public.public_navbar')
 @endsection
 
-@section('body')
-<h1>Ciao</h1>
 
+@section('body')
+  <img src="https://img.icons8.com/nolan/96/question-mark.png"/>
+  <img src="https://img.icons8.com/dusk/64/000000/coins.png"/>
+  <img src="https://img.icons8.com/nolan/96/wedding-gift.png"/>
+  <img src="https://img.icons8.com/wired/64/000000/friends.png"/>
+  <img src="https://img.icons8.com/nolan/100/combo-chart.png"/>
 @endsection
