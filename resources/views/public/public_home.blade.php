dc405f99118242f177c5749205f40a128592b49f
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-02-16 16:01:40 +0100

    try 3

--- resources/views/public/public_home.blade.php
+++ resources/views/public/public_home.blade.php
@@ -14,47 +14,62 @@
   <div class="container-fluid">
 
     <div class="row">
-    
-        <div class="col-lg-12">
-          </br>
+    </br>
+        <div class="col-lg-11">
+          
           <div class="jumbotron">
           <h1 class="display-4 main_title">Gift Tracker</h1>
             <p class="lead">Hai mai avuto problemi nel fare regali od acquisti personali che richiedessero una spesa comune? E' difficile tener traccia di chi 
             ha già saldato il suo debito e chi no? Se la risposta è si, allora sei nel posto giusto!</p>
-            <img src="https://img.icons8.com/nolan/96/question-mark.png"/> 
+            
               <hr class="my-4">
               <p>Gift Traker è il sito che ti permette di seguire ogni passaggio dalla creazione di una lista di oggetti all'acquisto degli stessi
-            in modo facile e veloce. Ogni membro dovrà registrarsi e potrà cosi nell'apposita sezione gestire tutte le transazioni per suddividere la 
-            spesa.</p>
+              in modo facile e veloce. Ogni membro dovrà registrarsi e potrà cosi nell'apposita sezione gestire tutte le transazioni per suddividere la spesa.</p>
               <p class="lead text-center">
               <button class="btn btn-secondary btn-lg" action="{{ route('login:page') }}" > Login </button>
               <button class="btn btn-secondary btn-lg" action="{{ route('register:page') }}" > Registrazione </button>
             </p>
           </div>
         </div>
+        <div class="col-lg-1 text-center">
+          <img src="https://img.icons8.com/nolan/96/question-mark.png"/> 
+        </div>
 
     </div>
-
+    </br></br></br>
     <div class="row">
-        <div class="col-lg-4">
+
+      <div class="col-lg-4">
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
             <img src="https://img.icons8.com/nolan/96/wedding-gift.png"/>
-            Crea un nuovo catalogo ed aggiungi quante più opzioni regalo desideri. 
+            <h1 class="display-4 main_title">Catalogo</h1>
+            <p class="lead">Crea un nuovo catalogo ed aggiungi quante più opzioni regalo desideri. Tu ed i tuoi amici potrete poi scegliere quale acquistare.</p>
+          </div>
         </div>
-        <div class="col-lg-4">
-          <img src="https://img.icons8.com/dusk/64/000000/coins.png"/>
-          Acquista da una tua lista o da quella di un amico con cui vuoi dividere la spesa
-        </div>
+      </div>
 
-        <div class="col-lg-4">
-          <img src="https://img.icons8.com/nolan/100/combo-chart.png"/>
-          Salda i tuoi debiti o controlla i tuoi creditori con un click
+      <div class="col-lg-4">
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <img src="https://img.icons8.com/dusk/64/000000/coins.png"/>
+            <h1 class="display-4 main_title">Acquisti</h1>
+            <p class="lead">Acquista da una tua lista o da quella di un amico con cui vuoi dividere la spesa. Ci penserà Gift Tracker a gestire tutto</p>
+          </div>
         </div>
+      </div>
+
+      <div class="col-lg-4">
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <img src="https://img.icons8.com/nolan/100/combo-chart.png"/>
+            <h1 class="display-4 main_title">Debitore o Creditore?</h1>
+            <p class="lead">Salda i tuoi debiti o controlla i tuoi creditori con un click</p>
+          </div>
+        </div>
+      </div>
+          
     </div>
-
-    <div class="row">
-      
-    </div>
-
 
   </div>
 
