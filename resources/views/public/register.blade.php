730bffcf1202db15ed5f70fb89a2f5423ed496b4
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-02-19 14:58:44 +0100

    ita in register eccetto password

--- resources/views/public/register.blade.php
+++ resources/views/public/register.blade.php
@@ -24,9 +24,9 @@
                     <h2 class="main_title">Gift Tracker</h2>
                     <form method="POST" action="{{ route('register:action') }}">
                         @csrf
-                        <input type="text"  required="required" class="form-control" name="name" placeholder="user name">
+                        <input type="text"  required="required" class="form-control" name="name" placeholder="Nome Utente">
                         <br>
-                        <input type="email" required="required" name="email"  class="form-control input" placeholder="email">
+                        <input type="email" required="required" name="email"  class="form-control input" placeholder="Email">
                         <br>
                         @include('input_new_password')
                         <input type="submit" class="btn btn-primary">
@@ -37,7 +37,7 @@
             <div class="card">
                 <div class="card-body jumbotron-color">
                     <p class="card-text  text-center">
-                        Have an account? <a href="{{ route('login') }}">Log in</a>
+                        Hai già un account? <a href="{{ route('login') }}">Accedi</a>
                     </p>
                 </div>
             </div>
