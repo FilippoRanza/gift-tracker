80a49c94a2091956d76469998329a633ae2f0d61
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-02-19 14:51:40 +0100

    spostato il bgcolor al body

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -18,14 +18,14 @@
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         
     </head>
-    <body>
+    <body class="base-background-color">
         <div>
             @yield('navbar')
         </div>
-        <div class="container-fluid base-background-color">
+        <div class="container-fluid">
             @yield('body')
         </div>
-        <div class="base-background-color">
+        <div>
             @yield('foot')
         </div>
         
