ce9f4ef559b5a60bf6a9857f01f101a8c01a8fad
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-03-03 16:47:48 +0100

    aggiunto container div a contributi

--- resources/views/private/contribute_list.blade.php
+++ resources/views/private/contribute_list.blade.php
@@ -7,63 +7,65 @@
     @if ($active_list)
         <h1>{{ __('contribute_list.active-title') }}</h1>
         <hr>
-        
-        <ul class="list-group">
-            @foreach ($active_list as $list)
-                <li class="list-group-item">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <h4>
-                                    <a href="{{ route('list:manage', ['id' => $list->id]) }}">
-                                        {{ $list->name }}
-                                    </a>
-                                </h4>
-                                <ul>
-                                    <li>{{ __('contribute_list.owner') }}: {{ $list->owner }} </li>
-                                </ul>
-                            </div>
-                            <div class="col-sm-3">
-                                <form method="POST" action="{{ route('list:unsubscribe') }}" >
-                                    @csrf
-                                    <input type="hidden" value="{{ $list->id }}" name="list"> 
-                                    @if ($list->poll)
-                                        <input type="submit" class="btn btn-warning form-control" value="{{ __('contribute_list.unsubscribe') }}" disabled>
-                                    @else
-                                        <input type="submit" class="btn btn-warning form-control" value="{{ __('contribute_list.unsubscribe') }}">    
-                                    @endif
-                                </form>
+        <div class="container">
+            <ul class="list-group">
+                @foreach ($active_list as $list)
+                    <li class="list-group-item">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <h4>
+                                        <a href="{{ route('list:manage', ['id' => $list->id]) }}">
+                                            {{ $list->name }}
+                                        </a>
+                                    </h4>
+                                    <ul>
+                                        <li>{{ __('contribute_list.owner') }}: {{ $list->owner }} </li>
+                                    </ul>
+                                </div>
+                                <div class="col-sm-3">
+                                    <form method="POST" action="{{ route('list:unsubscribe') }}" >
+                                        @csrf
+                                        <input type="hidden" value="{{ $list->id }}" name="list"> 
+                                        @if ($list->poll)
+                                            <input type="submit" class="btn btn-warning form-control" value="{{ __('contribute_list.unsubscribe') }}" disabled>
+                                        @else
+                                            <input type="submit" class="btn btn-warning form-control" value="{{ __('contribute_list.unsubscribe') }}">    
+                                        @endif
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
-            @endforeach
-        </ul>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     @endif
     @if ($archived_list)    
         <h1>{{ __('contribute_list.archived-title') }}</h1>
         <hr>
-
-        <ul class="list-group">
-            @foreach ($archived_list as $list)
-                <li class="list-group-item">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <h4>
-                                    <a href="{{ route('list:old', ['id' => $list->id]) }}">
-                                        {{ $list->name }}
-                                    </a>
-                                </h4>
-                                <ul>
-                                    <li>{{ __('contribute_list.owner') }}: {{ $list->owner }} </li>
-                                </ul>
+        <div class="container">
+            <ul class="list-group">
+                @foreach ($archived_list as $list)
+                    <li class="list-group-item">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <h4>
+                                        <a href="{{ route('list:old', ['id' => $list->id]) }}">
+                                            {{ $list->name }}
+                                        </a>
+                                    </h4>
+                                    <ul>
+                                        <li>{{ __('contribute_list.owner') }}: {{ $list->owner }} </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                
-                </li>
-            @endforeach
-        </ul>
+                    
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     @endif
 @endsection
