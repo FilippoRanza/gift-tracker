ba0f327e752e5def3585f3e753b0e29235037bf5
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-03-03 18:49:01 +0100

    grafica centrata manage list

--- resources/views/private/manage_list.blade.php
+++ resources/views/private/manage_list.blade.php
@@ -10,14 +10,16 @@
         @includeWhen(isset($guest_err), 'error', ['message' => __('manage_list.guest-err', ['guest' => $guest_err, 'list' => $list->name])])
     </div>
 
-    <ul class="nav nav-tabs" role="tablist" >
-        <li class="nav-item"><a class="nav-link" id="item-link" onclick="set_current('item', 'item-link');" data-toggle="tab" href="#item">{{ __('manage_list.item-tab') }}</a></li>
-        <li class="nav-item"><a class="nav-link" id="guest-link" onclick="set_current('guest', 'guest-link');" data-toggle="tab" href="#guest">{{ __('manage_list.guest-tab') }}</a></li>
+    <div class="container">
+        <ul class="nav nav-tabs" role="tablist" >
+            <li class="nav-item"><a class="nav-link" id="item-link" onclick="set_current('item', 'item-link');" data-toggle="tab" href="#item">{{ __('manage_list.item-tab') }}</a></li>
+            <li class="nav-item"><a class="nav-link" id="guest-link" onclick="set_current('guest', 'guest-link');" data-toggle="tab" href="#guest">{{ __('manage_list.guest-tab') }}</a></li>
 
-        @if (isset($guest_only_handle))
-            <li class="nav-item"><a class="nav-link" id="settings-link" onclick="set_current('settings', 'settings-link');" data-toggle="tab"  href="#settings">{{ __('manage_list.settings-tab') }}</a></li>    
-        @endif
-    </ul>
+            @if (isset($guest_only_handle))
+                <li class="nav-item"><a class="nav-link" id="settings-link" onclick="set_current('settings', 'settings-link');" data-toggle="tab"  href="#settings">{{ __('manage_list.settings-tab') }}</a></li>    
+            @endif
+        </ul>
+    </div>
     <br>
 
     <div class="container md-3">
