c9cebe6fe919b31232888aca40783ac7aa848dbe
Filippo Ranza <filipporanza@gmail.com>
2021-03-03 18:00:19 +0100

    add italian version

---
+++ resources/lang/it/list_description.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'guest-only-description' => 'L\'opzione "Non partecipo alla spesa" permette di creare una lista in cui l\'acquisto degli oggetti al suo interno è permesso solo ai partecipanti che inserirai. Tu non potrai effettuare acquisti, ma solo gestire la lista.'
+];
