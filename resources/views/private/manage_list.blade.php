7f56c5d4ebdca258ec44138129e2d01f4afba4a9
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-03-03 18:55:25 +0100

    grafica gestione liste v1

--- resources/views/private/manage_list.blade.php
+++ resources/views/private/manage_list.blade.php
@@ -2,8 +2,8 @@
 
 
 @section('body')
-    <h3>{{ $list->name }} - {{ $user->name }}</h3>
-    
+    <h1>Gestione Lista: {{ $list->name }}</h1>
+    <hr>
     <div>
         @includeWhen(isset($item_err), 'error', ['message' => __('manage_list.item-err', ['item' => $item_err, 'list' => $list->name])])
         @includeWhen(isset($user_err), 'error', ['message' => __('manage_list.user-err', ['name' => $user_err])])
