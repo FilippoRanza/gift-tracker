f26f75d7eed1b534371375dd98c6343c6cb61a8e
Filippo Ranza <filipporanza@gmail.com>
2021-03-05 20:39:23 +0100

    list user names under add guest field

--- resources/views/private/list_manager/list_guest.blade.php
+++ resources/views/private/list_manager/list_guest.blade.php
@@ -3,11 +3,13 @@
         
             <div class="card">
                 <div class="card-body">
-                    <h3 class="card-title">{{ __('list_guest.add-title') }}</h3>
+                    <h3 class="card-title">{{ __('list_guest.add-title') }} ffff</h3>
                     <form  method="POST" class="input-group form-inline" action="{{ route('list:add_guest') }}">
                         @csrf
-                        <input type="hidden" value="{{ $list->id }}" name="list">
-                        <input type="text" id="guest-name" required="required" class="form-control" name="name" autofocus placeholder="{{ __('list_guest.input-name') }}">
+                        <input type="hidden" id="list-id" value="{{ $list->id }}" name="list">
+                        <input type="text" autocomplete="off" id="guest-name" required="required" class="autocomplete form-control" data-toggle="dropdown"  name="name" autofocus placeholder="{{ __('list_guest.input-name') }}">
+                        <ul class="dropdown-menu" id="dropdown-menu" role="menu">
+                        </ul>
                         <input type="submit" id="add-guest" class="btn btn-secondary mb-2 pl-2" value="{{ __('list_guest.submit') }}" >
                     </form>
                 </div>
@@ -16,7 +18,7 @@
         <br> 
     @endif
     
-    
+    <meta name="csrf-token" content="{{ csrf_token() }}"> 
         <div class="card">
             <div class="card-body">
                 <h4>{{ __('list_guest.current-title') }}</h4>
@@ -51,3 +53,32 @@
         </div>
    
 
+<script>
+console.log('DDDDD');
+$('#guest-name').on('input', function() {
+        $('#dropdown-menu').empty();
+        var new_name = $('#guest-name').val();
+        var list_id = $('#list-id').val();
+        var post_data =  {
+            'current' : new_name,
+            'list_id': list_id,
+            '_token': $('meta[name=csrf-token]').attr('content')
+        };
+        console.log(post_data);
+        var url = "{{ route('list:list-users') }}";
+        $.ajax({
+            url: url,
+            data: post_data,
+            type: "post",
+            dataType: "json",
+        }).done(function (json) {
+            var names = json['names'];
+            var drop_down = $('#dropdown-menu');
+            names.forEach(function(elem) {
+                console.log(elem);
+                drop_down.append(`<li>${elem}</li>`)
+            });
+        });
+        
+    });
+</script>
