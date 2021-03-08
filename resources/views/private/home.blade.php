962c2c4815c3a4ebc1dd925a8e1ee6f23aa757c3
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-03-08 15:04:38 +0100

    hove ripristinata

--- resources/views/private/home.blade.php
+++ resources/views/private/home.blade.php
@@ -5,15 +5,15 @@
     <hr>
     <p class="small text-secondary">Questa è la home di Gift Traker. Da qui puoi avere accesso e controllare tutto quello che ti serve per i tuoi acquisti</p>
 
-    <div class="container">
-        <div class="card">
+     <!--<div class="container">
+       <div class="card">
             <div class="card-body">
                 <a class="card-title" href="{{ route('list:show') }}">{{ __('nav_bar.user-list') }} </a>
                 <p class="small text-secondary"> {{ __('info.list-info')  }} </p>
             </div>
         </div>        
 
-        <!--<div class="card">
+        <div class="card">
             <div class="card-body">
                 <h3 class="card-title"><a href="{{ route('list:guest') }}">{{ __('nav_bar.contribute-list') }} </a></h3>
                 <p class="small text-secondary"> {{ __('info.contribute-info')  }} </p>
@@ -39,11 +39,11 @@
                 <h3 class="card-title"><a href="{{ route('settings:index') }}">{{ __('nav_bar.user-settings') }} </a></h3>
                 <p class="small text-secondary"> {{ __('info.settings')  }} </p>
             </div>
-        </div> -->   
+        </div>  
 
 
 
-    </div>
+    </div>-->  
 
     <ul>
         <li>
