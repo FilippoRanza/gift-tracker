2b0d38eceb12a2f7578224e98b3a1c871f292104
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-03-08 14:58:22 +0100

    aggiunte card alla home

--- resources/views/private/home.blade.php
+++ resources/views/private/home.blade.php
@@ -8,10 +8,41 @@
     <div class="container">
         <div class="card">
             <div class="card-body">
-                <h3 class="card-title">{{ __('nav_bar.user-list') }}</h3>
+                <h3 class="card-title"><a href="{{ route('list:show') }}">{{ __('nav_bar.user-list') }} </a></h3>
                 <p class="small text-secondary"> {{ __('info.list-info')  }} </p>
             </div>
         </div>        
+
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title"><a href="{{ route('list:guest') }}">{{ __('nav_bar.contribute-list') }} </a></h3>
+                <p class="small text-secondary"> {{ __('info.contribute-info')  }} </p>
+            </div>
+        </div>    
+
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title"><a href="{{ route('purchase:list') }}">{{ __('nav_bar.user-purchase') }} </a></h3>
+                <p class="small text-secondary"> {{ __('info.purchase-info')  } </p>
+            </div>
+        </div>    
+
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title"><a href="{{ route('debt:list') }}">{{ __('nav_bar.user-debt') }} </a></h3>
+                <p class="small text-secondary"> {{ __('info.debt-info')  }} </p>
+            </div>
+        </div>    
+
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title"><a href="{{ route('settings:index') }}">{{ __('nav_bar.user-settings') }} </a></h3>
+                <p class="small text-secondary"> {{ __('info.settings')  }} </p>
+            </div>
+        </div>    
+
+
+
     </div>
 
     <ul>
