2e530a5eada599e45b815c2488066b056d5d7220
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-03-09 17:06:45 +0100

    modifica nomi bottoni acquisto

--- resources/lang/en/purchase_list.php
+++ resources/lang/en/purchase_list.php
@@ -5,9 +5,9 @@ return [
     'title' => 'My purchase list',
     'for' => 'For',
     'price' => 'Price',
-    'delete' => 'Delete',
-    'void' => 'Void',
-    'info-button' => 'Info', 
+    'delete' => 'Delete from History',
+    'void' => 'Void Purchase',
+    'info-button' => 'Purchase Info', 
     'info' => 'In this section you can view the products purchased by you for each list',
     'no-current-puchase' => 'At the current time there aren\'t purchases in your history'
 ];
--- resources/lang/it/purchase_list.php
+++ resources/lang/it/purchase_list.php
@@ -5,9 +5,9 @@ return [
 	'title' => 'I miei Acquisti',
 	'for' => 'Per',
 	'price' => 'Prezzo',
-	'delete' => 'Rimuovi',
-	'void' => 'Annulla',
-	'info-button' => 'Info',
+	'delete' => 'Cancella dallo Storico',
+	'void' => 'Annulla Acquisto',
+	'info-button' => 'Informazioni Acquisto',
 	'info' => 'In questa sezione puoi visualizzare i prodotti comprati da te per ogni lista',
 	'no-current-puchase' => 'Al momento non ci sono acquisti nel tuo storico'
 ];
