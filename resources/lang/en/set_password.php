516b4e02068abd500cd5189cacbf2429d687aa5e
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-03-09 17:23:46 +0100

    password key

--- resources/lang/en/set_password.php
+++ resources/lang/en/set_password.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'new-password' => 'New Password',
-    'confirm-password' => 'Confirm new Password',
+    'new-password' => ':key Password',
+    'confirm-password' => 'Confirm :key Password',
     'password-msg' => 'A good password should contain at least one capital case letter, one lower case letter, a number , one  special symbol from _ ( ) & % $ £ ! and at lease six characters',
     'password-match' => 'Passwords must match',
+    'new-key' => 'New',
 ];
--- resources/lang/it/set_password.php
+++ resources/lang/it/set_password.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'new-password' => 'Nuova Password',
-    'confirm-password' => 'Conferma nuova Password',
+    'new-password' => ':key Password',
+    'confirm-password' => 'Conferma :key Password',
     'password-msg' => 'Una buona password dovrebbe contenere almeno una lettera minuscola, una mauscola, un numero, un carattere speciale di questi _ ( ) & % $ £ ! e lunga almeno sei caratteri',
     'password-match' => 'Le password devono coincidere',
+    'new-key' => 'Nuova',
 ];
