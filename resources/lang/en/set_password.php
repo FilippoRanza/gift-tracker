0bd0455e219ed30c4da618bbd8688a5ed71de796
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-03-09 17:20:30 +0100

    nome tecnolesi password

--- resources/lang/en/set_password.php
+++ resources/lang/en/set_password.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'new-password' => 'Password',
-    'confirm-password' => 'Confirm Password',
+    'new-password' => 'New Password',
+    'confirm-password' => 'Confirm new Password',
     'password-msg' => 'A good password should contain at least one capital case letter, one lower case letter, a number , one  special symbol from _ ( ) & % $ £ ! and at lease six characters',
     'password-match' => 'Passwords must match',
 ];
--- resources/lang/it/set_password.php
+++ resources/lang/it/set_password.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'new-password' => 'Password',
-    'confirm-password' => 'Conferma Password',
+    'new-password' => 'Nuova Password',
+    'confirm-password' => 'Conferma nuova Password',
     'password-msg' => 'Una buona password dovrebbe contenere almeno una lettera minuscola, una mauscola, un numero, un carattere speciale di questi _ ( ) & % $ £ ! e lunga almeno sei caratteri',
     'password-match' => 'Le password devono coincidere',
 ];
