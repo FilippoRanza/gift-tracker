7d56a8882aab4b318cf0d70155f59fbb9c7130b2
Filippo Ranza <filipporanza@gmail.com>
2021-03-10 16:10:25 +0100

    improve debits

--- resources/lang/en/debt_home.php
+++ resources/lang/en/debt_home.php
@@ -7,7 +7,7 @@ return [
     'credit' => 'Credit',
     'mark' => 'Mark as Settle',
     'confirm' => 'Waiting for Confirm',
-    'settle' => 'Settle',
-    'info' => 'Here you can see all the debts and credits you have towards other users',
+    'settle' => 'Confirm credit Settle',
+    'info' => 'Here you can see all the debts and credits you have towards other users, in this page you can confirm your credit settle or ask the creditor to confirm your settlement',
     'no-current-debt' => 'You have not debts or credits at the moment'
 ];
--- resources/lang/it/debt_home.php
+++ resources/lang/it/debt_home.php
@@ -5,9 +5,9 @@ return [
 	'notify' => 'Notifica|Notifiche',
 	'debt' => 'Debito',
 	'credit' => 'Credito',
-	'mark' => 'Segna come Regolato',
+	'mark' => 'Segna come Risarcito',
 	'confirm' => 'Attendo la Conferma',
-	'settle' => 'Regolato',
-	'info' => 'Qui sono visibili tutti i debiti e crediti che hai nei confronti degli altri utenti',
+	'settle' => 'Conferma come Risarcito',
+	'info' => 'Qui sono visibili tutti i debiti e crediti che hai nei confronti degli altri utenti. In questa pagina puoi regolare i tuoi crediti o chiedere al creditore di confermare la regolazione del debito',
 	'no-current-debt' => 'Non hai alcun debito o credito in questo momento'
 ];
