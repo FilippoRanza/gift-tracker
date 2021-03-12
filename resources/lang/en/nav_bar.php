c94627d1a4a3d4c10d193850768347c2b2b05842
Kevin Cavazzini <k.cavazzini@studenti.unibs.it>
2021-03-12 10:53:41 +0100

    change name to navbar deb

--- resources/lang/en/nav_bar.php
+++ resources/lang/en/nav_bar.php
@@ -5,7 +5,7 @@ return [
     'user-list' => 'My Lists',
     'contribute-list' => 'Contribute List',
     'user-purchase' => 'My Purchases',
-    'user-debt' => 'My Debt',
+    'user-debt' => 'My Debt and Credit',
     'user-settings' => 'Settings',
     'logout' => 'Logout',
     'login' => 'Login',
--- resources/lang/it/nav_bar.php
+++ resources/lang/it/nav_bar.php
@@ -5,7 +5,7 @@ return [
 	'user-list' => 'Le mie Liste',
 	'contribute-list' => 'I miei Contributi',
 	'user-purchase' => 'I miei Acquisti',
-	'user-debt' => 'Debiti',
+	'user-debt' => 'Debiti e Crediti',
 	'user-settings' => 'Impostazioni',
 	'logout' => 'Esci',
 	'login' => 'Accedi',
